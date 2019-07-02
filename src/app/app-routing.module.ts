7715fc939a9d400514e194417bf765bcc25239f0
Angel Antezana <ghehahanghel@gmail.com>
2019-07-02 11:38:13 -0400

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  
+  //Registro de rutas de los modulos
+  {
+    path:'paquetes',
+    loadChildren:() => import('./paquete/paquete.module').then(mod => mod.PaqueteModule)
+ 
+  }
 ];
 
 @NgModule({
--- src/app/paquete/paquete-routing.module.ts
+++ src/app/paquete/paquete-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PaqueteListaComponent } from './paquete-lista/paquete-lista.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'lista',
+    component: PaqueteListaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
