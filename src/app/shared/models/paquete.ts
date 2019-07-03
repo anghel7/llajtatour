6249b5a296cff085e1574c091ff73269e1b1a5c2
Angel Antezana <ghehahanghel@gmail.com>
2019-07-03 10:32:46 -0400

    creando modelo paquete

---
+++ src/app/shared/models/paquete.ts
@@ -0,0 +1,14 @@
+export class Paquete {
+    id:number;
+    destino:string;
+    precio:number;
+    guia:string;
+    fechaPartida:string;
+}
+/**
+ * destino: 'Liruini',
+ * descripcion: 'Este paquete ..........'
+ * precio: 100
+ * fechaIni: 'may 27 2019'
+ * guia:'Juan Perez'
+ */
