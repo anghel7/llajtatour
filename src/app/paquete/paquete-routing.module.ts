583ae306b59f35531999e6be0766658db3c96455
Angel Antezana <ghehahanghel@gmail.com>
2019-07-03 12:21:05 -0400

    creando vista para el formulario crear

--- src/app/paquete/paquete-routing.module.ts
+++ src/app/paquete/paquete-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PaqueteListaComponent } from './paquete-lista/paquete-lista.component'
+import { PaqueteCrearComponent } from './paquete-crear/paquete-crear.component';
 
 const routes: Routes = [
   {
     path:'lista',
     component: PaqueteListaComponent
+  },
+  {
+    path:'crear',
+    component: PaqueteCrearComponent
   }
 ];
 
