1190723b0d60de4e991e943feeb6f3bc72948a10
Angel Antezana <ghehahanghel@gmail.com>
2019-07-09 11:11:12 -0400

    recuperando por id

--- src/app/paquete/paquete-detalle/paquete-detalle.component.ts
+++ src/app/paquete/paquete-detalle/paquete-detalle.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PaqueteService } from 'src/app/shared/services/paquete.service';
+
 
 @Component({
   selector: 'app-paquete-detalle',
@@ -8,18 +10,26 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PaqueteDetalleComponent implements OnInit {
 
-  constructor(private activatedroute:ActivatedRoute) { }
+  constructor(private activatedroute: ActivatedRoute,
+    private paqueteservice: PaqueteService) { }
 
   ngOnInit() {
     //Observable
     this.activatedroute.params
       .subscribe(
         (params) => {
-          console.log('Paramas repuerado: ', params);
-          
-         },
+          this.paqueteservice.recuperarPaqueteId(params.id)
+            .subscribe(
+              (response)=>{
+                console.log("Paquete: ", response);                
+              },
+              (error)=>{
+                console.log("error al recupera el paquete por id: ",error);                
+              }
+            );
+        },
         (error) => {
-          console.log('Error al recuperar params');          
+          console.log('Error al recuperar params');
         }
       );
   }
--- src/app/shared/services/paquete.service.ts
+++ src/app/shared/services/paquete.service.ts
@@ -8,17 +8,20 @@ import { Paquete } from '../models/paquete';
 })
 export class PaqueteService {
 
-  constructor(private httpclient:HttpClient) { }
+  constructor(private httpclient: HttpClient) { }
 
   /**Crear paquete POST*/
-  crearPaquete(data:any):Observable<Paquete>{
-    return this.httpclient.post<Paquete>("https://llajtatour.herokuapp.com/api/paquetes/",data);
+  crearPaquete(data: any): Observable<Paquete> {
+    return this.httpclient.post<Paquete>("https://llajtatour.herokuapp.com/api/paquetes/", data);
   }
   /**Listar paquetes GET*/
-  listarPaquetes():Observable<Paquete[]>{
+  listarPaquetes(): Observable<Paquete[]> {
     return this.httpclient.get<Paquete[]>("https://llajtatour.herokuapp.com/api/paquetes/");
   }
   /**Recuperar paquete  por Id GET*/
+  recuperarPaqueteId(id: any): Observable<Paquete> {
+    return this.httpclient.get<Paquete>("https://llajtatour.herokuapp.com/api/paquetes/" + id + "/");
+  }
   /**Editar paquete  por Id PUT*/
   /**Eliminar paquete  por Id DELETE*/
 }
